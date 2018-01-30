90eeaaf41a3858979593478e11d5620209e0eb36
purusothpuru <35653361+purusothpuru@users.noreply.github.com>
2018-01-30 16:07:02 +0530

    Create so.c

---
+++ so.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	    int a,b;clrscr();
+	     printf("ENTER THE NO.OF TIMES TO PRINT\n");
+	     scanf("%d",&a);
+	    for(b=0;b<a;b++)
+	    {
+	     printf("Hello\n");
+	
+	}
+	    
+	    getch();
+	}
