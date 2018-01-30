8a679d1895b633818ee144ba39c10edf702bd2ef
purusothpuru <35653361+purusothpuru@users.noreply.github.com>
2018-01-30 16:01:52 +0530

    Create cp.c

---
+++ cp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+	void main()
+	{
+	    char i;
+	    printf("enter the i value");
+	    scanf("%c",&i);
+	    if((i>='a'&&i<='z')||(i>='A'&&i<='Z'))
+	    printf("\nalphabet");
+	    else
+	    printf("not for alphabet");
+	}
