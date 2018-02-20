741fe0cca6d2efe6df05e69ea3c12383bc2841f4
purusothpuru <35653361+purusothpuru@users.noreply.github.com>
2018-02-20 16:22:19 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+	char a[50];
+	int n,i,count=1;
+	printf("enter the input sentence \n");
+	gets(a);
+	n=strlen(a);
+	printf("\n the length of sentence is %d",n);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]=='\.')
+		{
+			count++;
+		}
+	}
+	printf("\n the no.of special character are %d",count);
+  return 0;
+}
