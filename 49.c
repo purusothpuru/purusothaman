9f734358aba9c11694f1ded88e0aeb9da3e2116b
purusothpuru <35653361+purusothpuru@users.noreply.github.com>
2018-03-02 15:01:06 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h> 
+void main() 
+{
+	int a,b,c;
+	printf("enter the value \n");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("the sum of value are \n %d",c);
+ 	getch();
+}
