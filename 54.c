f4225e46424931060f71676321dc45ab59f8c0ef
purusothpuru <35653361+purusothpuru@users.noreply.github.com>
2018-03-13 16:11:38 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d",n);
+}
+else
+{
+printf("%d",n-1);
+}
+getch();
+}
